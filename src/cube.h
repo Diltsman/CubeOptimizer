77c93c9e2d74b793e1459e700d621f8bfedd0797
Daniel Dilts <diltsman@gmail.com>
2015-08-29 20:06:29 -0700

    typedef char[3][3] for a face

--- src/cube.h
+++ src/cube.h
@@ -1,14 +1,18 @@
 #ifndef SRC_CUBE_H
 #define SRC_CUBE_H
 
-struct cube {
-  char c[6][3][3];
-  char (*right)[3][3] = &c[0];
-  char (*up)[3][3] = &c[1];
-  char (*front)[3][3] = &c[2];
-  char (*left)[3][3] = &c[3];
-  char (*down)[3][3] = &c[4];
-  char (*back)[3][3] = &c[5];
+class cube {
+  using face = char[3][3];
+
+public:
+private:
+  face c[6];
+  face *mRight = c + 0;
+  face *mUp = c + 1;
+  face *mFront = c + 2;
+  face *mLeft = c + 3;
+  face *mDown = c + 4;
+  face *mBack = c + 5;
 };
 
 #endif
