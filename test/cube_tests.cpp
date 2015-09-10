47cc01423cbe5b6e79742e16540b4c5bc969150c
Daniel Dilts <diltsman@gmail.com>
2015-09-10 15:17:29 -0700

    Add test cube that sets each position to a unique value

--- test/cube_tests.cpp
+++ test/cube_tests.cpp
@@ -1,3 +1,23 @@
 #include <gtest/gtest.h>
+#include "../src/cube.h"
 
-TEST(CubeTests, Test) {}
+class TestCube : public cube {
+public:
+  TestCube() {
+    for (int i = 0; i < 6; ++i) {
+      for (int j = 0; j < 3; ++j) {
+        for (int k = 0; k < 3; ++k) {
+          char value = i << 4;
+          value |= j << 2;
+          value |= k;
+          SetPosition(static_cast<Face>(i), j, k, value);
+        }
+      }
+    }
+  }
+};
+
+TEST(CubeTests, RightCW) {
+  TestCube test;
+  test.RightCW();
+}
