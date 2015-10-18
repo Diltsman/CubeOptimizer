2102421fd88ccd6e2fbed22fe7dbd43f40821626
Daniel Dilts <diltsman@gmail.com>
2015-10-17 17:16:22 -0700

    Implement LeftCW

--- src/cube.cpp
+++ src/cube.cpp
@@ -30,7 +30,20 @@ void cube::RightCCW() {
   Cycle(front[1][2], down[1][2], back[1][0], up[1][2]);
 }
 
-void cube::LeftCW() {}
+void cube::LeftCW() {
+  face &left = Left();
+  face &up = Up();
+  face &front = Front();
+  face &back = Back();
+  face &down = Down();
+  // Corners
+  Cycle(left[0][0], left[0][2], left[2][2], left[2][0]);
+  Cycle(up[0][0], front[0][0], down[0][0], back[2][2]);
+  Cycle(up[2][0], front[2][0], down[2][0], back[0][2]);
+  // Edges
+  Cycle(left[0][1], left[1][2], left[2][1], left[1][0]);
+  Cycle(up[1][0], front[1][0], down[1][0], back[1][2]);
+}
 
 cube::face &cube::GetFace(Face face) {
   switch (face) {
@@ -49,6 +62,20 @@ cube::face &cube::GetFace(Face face) {
   }
   throw std::invalid_argument{"Must request a valid face"};
 }
+
+struct Value {
+   Value(int i, int j, int k) : i(static_cast<Face>(i)), j(j), k(k) {}
+   Face i;
+   int j;
+   int k;
+   Value(char c) : i(static_cast<Face>(c >> 4)), j((c >> 2) & 0x3), k(c & 0x3) {}
+   operator char() const {
+      char value = static_cast<int>(i) << 4;
+      value |= j << 2;
+      value |= k;
+      return value;
+   }
+};
 
 bool operator==(const cube &a, const cube &b) {
   bool equal = true;
